cc7a8259e14155a95d5e344bad7db6c41f946b44
Ravi <ravikhichar9715@gmail.com>
2026-02-23 16:48:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+app= Flask(__name___)
+
+@app.get("/")
+def hello():
+    return jsonify(
+        message="Just a dummy app created by RAVI",
+        tip= "Built with the Flask ,shipped by jenkins,running in docker (first ci/cd deployment)"
+    )
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
